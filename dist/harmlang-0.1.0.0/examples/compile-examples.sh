58d9ea38657df8793249cd61db6c2498c3bb403a
Louis Rassaby <louis.rassaby@gmail.com>
2014-11-07 14:29:06 -0500

    adding with dependency on base 4.5

---
+++ dist/harmlang-0.1.0.0/examples/compile-examples.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/sh
+ghc ../src/HarmLang/Types.hs  \
+    ../src/HarmLang/Parser.hs  \
+    ../src/HarmLang/Interpreter.hs  \
+    ../src/HarmLang/InitialBasis.hs  \
+    cycle.hs
+
+ghc ../src/HarmLang/Types.hs  \
+    ../src/HarmLang/Parser.hs  \
+    ../src/HarmLang/Interpreter.hs  \
+    ../src/HarmLang/InitialBasis.hs  \
+    transpose.hs
+
+ghc ../src/HarmLang/Types.hs  \
+    ../src/HarmLang/Parser.hs  \
+    ../src/HarmLang/Interpreter.hs  \
+    ../src/HarmLang/InitialBasis.hs  \
+    melody.hs
